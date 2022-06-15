18983e58e644f1fc2c65ffcc9d0f8fbf24d7a8da
Matt Snow <matt@mattsn0w.com>
2022-06-14 22:07:11 -0600

    vm start script for VMs not running.

--- roles/hypervisor/templates/start-vms-that-are-shut-off.sh
+++ roles/hypervisor/templates/start-vms-that-are-shut-off.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 VIRSH=/usr/bin/virsh
-
+EXIT_CODE=0
 for VMID in $( ${VIRSH} list --all | grep 'shut off' | awk '{print $2}' ); do
   echo "attempting to start $VMID as it is currently shutdown..."
   $VIRSH start $VMID
+  EXIT_CODE=$[ ${EXIT_CODE} + $? ]
 done
+exit ${EXIT_CODE}
 
